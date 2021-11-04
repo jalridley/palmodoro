b6d56ed6e6ef227095dde69a432d7b9752edc528
Jalynn Ridley <jallywally@gmail.com>
2021-11-04 17:43:57 -0400

    created App.js

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+    return <div>Pomodoro</div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
-import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-// ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
