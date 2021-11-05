b37af9c24d564b318b203b3d572dc7e809e00d07
Jalynn Ridley <jallywally@gmail.com>
2021-11-05 14:36:07 -0400

    set up container in App.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const App = () => {
-    return <div>Pomodoro</div>;
+    return (
+        <div className="container">
+            <h1>Pomodoro</h1>
+            <h2>Ready...set...go!</h2>
+        </div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './components/App.css';
 
 // from index.html
 ReactDOM.render(<App />, document.querySelector('#root'));
