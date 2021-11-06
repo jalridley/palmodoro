e4ffe093b69341ca6b08a9b537ca305d0b4d9f57
Jalynn Ridley <jallywally@gmail.com>
2021-11-06 15:09:13 -0400

    installed and setup sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './components/App.css';
+import './components/App.scss';
 
 // from index.html
 ReactDOM.render(<App />, document.querySelector('#root'));
