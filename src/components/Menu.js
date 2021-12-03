43b416e86f0954d4342916b90e91529ac3e16973
Jalynn Ridley <jallywally@gmail.com>
2021-12-03 15:00:55 +0100

    menu-icon removed from div, sized, positioned

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,9 +2,5 @@ import React from 'react';
 import menu from '../menu.svg';
 
 export const Menu = () => {
-    return (
-        <div className="menu-icon">
-            <img src={menu} alt="menu" />
-        </div>
-    );
+    return <img className="menu-icon" src={menu} alt="menu" />;
 };
