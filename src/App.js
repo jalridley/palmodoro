5047306d3923ba88417e7f6a4c284162ae88d15d
Jalynn Ridley <jallywally@gmail.com>
2021-12-28 09:57:49 -0500

    created showMenu toggle in menu component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import { Header } from './components/Header';
 import { ImageContainer } from './components/ImageContainer';
 import { Menu } from './components/Menu';
 import { Timer } from './components/Timer';
-// import { Controls } from './components/Controls';
 
 import './App.scss';
-// import image from '../palm-flam.jpg';
 
 const App = () => {
     return (
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 import menu from '../menu.svg';
 
 export const Menu = () => {
-    return <img className="menu-icon" src={menu} alt="menu" />;
+    const [showMenu, setShowMenu] = useState(false);
+
+    const isMenuOpen = () => {
+        setShowMenu(!showMenu);
+        console.log('inside is menu open function');
+    };
+    console.log(showMenu);
+
+    return (
+        <div>
+            {/* if showMenu is true, show menus svg and the dropdown menu
+            else just show the svg */}
+            <img
+                className="menu-icon"
+                src={menu}
+                alt="menu"
+                onClick={() => isMenuOpen()}
+            />
+            {showMenu ? (
+                <div className="menu">
+                    <p style={{ color: 'white' }}>menu open</p>
+                </div>
+            ) : (
+                <p>showMenu false</p>
+            )}
+        </div>
+    );
 };
